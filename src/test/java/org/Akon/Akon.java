c2af8021aeb4574217d00ac8d5d9627484dda4e1
sasikiran0105 <sasikiransusanth@gmail.com>
2022-05-27 12:17:51 +0530

    first commit

---
+++ src/test/java/org/Akon/Akon.java
@@ -0,0 +1,20 @@
+package org.Akon;
+
+public class Akon {
+	
+	public void method1() {
+		System.out.println("method1");
+	}
+	public void method2() {
+		System.out.println("method2");
+	}
+	public void method3() {
+		System.out.println("method3");
+	}
+	public void method4() {
+		System.out.println("method4");
+	}
+	
+	
+
+}
